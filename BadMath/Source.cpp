3e546093954ad045fc36c66cc5d682f79903797b
Qi Zhang <jacky780203@sina.com>
2021-11-16 00:21:37 -0500

    Fixed expression precedence & "n2" -> "n3"

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
@@ -13,7 +13,7 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
